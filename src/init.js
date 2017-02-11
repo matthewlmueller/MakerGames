f12323693465d322b587a79750092a2cfd4a7440
John J. Gallagher <john.gallagher@tufts.edu>
2017-02-11 10:24:46 -0800

    a dummy piece now appears on the board

--- src/init.js
+++ src/init.js
@@ -3,12 +3,13 @@ window.onload = function() {
 
 
         function preload () {
-                game.load.image('board', 'assets/board.jpg');
-                game.load.image('piece1', 'assets/pieces/piece1.png')
+                game.load.image('board_image', 'assets/board.jpg');
+                game.load.image('piece1_image', 'assets/pieces/piece1.png');
         }
 
         function create () {
-                var board = game.add.sprite(game.world.centerX, game.world.centerY, 'board');
+                var board  = game.add.sprite(game.world.centerX, game.world.centerY, 'board_image');
+                var piece1 = game.add.sprite(900, 900, 'piece1_image'); 
                 board.anchor.setTo(0.5, 0.5);
         }
 
