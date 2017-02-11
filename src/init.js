1c274cfdfedb9224483950c7d20740dc2bf5bfa4
John J. Gallagher <john.gallagher@tufts.edu>
2017-02-11 10:16:32 -0800

    added a dummy piece from freegameart

--- src/init.js
+++ src/init.js
@@ -1,19 +1,15 @@
 window.onload = function() {
+        var game = new Phaser.Game(1000, 1000, Phaser.AUTO, '', { preload: preload, create: create });
 
 
-    var game = new Phaser.Game(1000, 1000, Phaser.AUTO, '', { preload: preload, create: create });
+        function preload () {
+                game.load.image('board', 'assets/board.jpg');
+                game.load.image('piece1', 'assets/pieces/piece1.png')
+        }
 
-    function preload () {
-
-        game.load.image('board', 'assets/board.jpg');
-
-    }
-
-    function create () {
-
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'board');
-        logo.anchor.setTo(0.5, 0.5);
-
-    }
+        function create () {
+                var board = game.add.sprite(game.world.centerX, game.world.centerY, 'board');
+                board.anchor.setTo(0.5, 0.5);
+        }
 
 };
