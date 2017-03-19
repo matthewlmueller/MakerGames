0914a6ce5f2fe1352695e3c6a356dd9a744ca9b8
John J. Gallagher <john.gallagher@tufts.edu>
2017-03-19 07:52:26 +0000

    Implementing posistions for all pieces

--- src/game_info.js
+++ src/game_info.js
@@ -23,7 +23,7 @@ function Game_info (data) {
                         "piece_2_y":825,
 
                         "piece_3_x":925,
-                        "piece_3_y":825,
+                        "piece_3_y":830,
 
                         "piece_4_x":830,
                         "piece_4_y":925,
@@ -42,102 +42,233 @@ function Game_info (data) {
                         "piece_2_x":725,
                         "piece_2_y":825,
 
-                        "piece_3_x":775,
+                        "piece_3_x":785,
                         "piece_3_y":825,
+
+                        "piece_4_x":680,
+                        "piece_4_y":785,
+
+                        "piece_5_x":725,
+                        "piece_5_y":785,
+
+                        "piece_6_x":785,
+                        "piece_6_y":925,
 
                 },
                 // Tile 3
                 {
                         "piece_1_x":520,
-                        "piece_1_y":825
+                        "piece_1_y":825,
+
+                        "piece_2_x":570,
+                        "piece_2_y":825,
+
+                        "piece_3_x":615,
+                        "piece_3_y":825,
+
+                        "piece_4_x":520,
+                        "piece_4_y":615,
+
+                        "piece_5_x":570,
+                        "piece_5_y":615,
+
+                        "piece_6_x":615,
+                        "piece_6_y":925,
+
                 },
                 // Tile 4
                 {
                         "piece_1_x":360,
-                        "piece_1_y":825
+                        "piece_1_y":825,
+
+                        "piece_2_x":410,
+                        "piece_2_y":825,
+
+                        "piece_3_x":455,
+                        "piece_3_y":825,
+
                 },
                 // Tile 5
                 {
-                        "piece_1_x":210,
-                        "piece_1_y":825
+                        "piece_1_x":215,
+                        "piece_1_y":825,
+
+                        "piece_2_x":260,
+                        "piece_2_y":825,
+
+                        "piece_3_x":320,
+                        "piece_3_y":825,
                 },
                 // Tile 6
                 {
                         "piece_1_x":50,
-                        "piece_1_y":825
+                        "piece_1_y":825,
+
+                        "piece_2_x":100,
+                        "piece_2_y":825,
+
+                        "piece_3_x":150,
+                        "piece_3_y":825,
                 },
         // First Column
                 // Tile 7
                 {
                         "piece_1_x":50,
-                        "piece_1_y":675
+                        "piece_1_y":675,        
+
+                        "piece_2_x":50,
+                        "piece_2_y":725,
+
+                        "piece_3_x":50,
+                        "piece_3_y":775,
                 },
                 // Tile 8
                 {
                         "piece_1_x":50,
-                        "piece_1_y":525
+                        "piece_1_y":525,
+
+                        "piece_2_x":50,
+                        "piece_2_y":575,
+
+                        "piece_3_x":50,
+                        "piece_3_y":630,
+
                 },
                 // Tile 9
                 {
                         "piece_1_x":50,
-                        "piece_1_y":360
+                        "piece_1_y":360,
+
+                        "piece_2_x":50,
+                        "piece_2_y":410,
+
+                        "piece_3_x":50,
+                        "piece_3_y":465,
                 },
                 // Tile 10
                 {
                         "piece_1_x":50,
-                        "piece_1_y":200
+                        "piece_1_y":200,
+
+                        "piece_2_x":50,
+                        "piece_2_y":250,
+
+                        "piece_3_x":50,
+                        "piece_3_y":300,
                 },
                 // Tile 11
                 {
                         "piece_1_x":50,
-                        "piece_1_y":50
+                        "piece_1_y":50,
+
+                        "piece_2_x":50,
+                        "piece_2_y":100,
+
+                        "piece_3_x":50,
+                        "piece_3_y":150,
                 },
         // Second Row
                 // Tile 12
                 {
                         "piece_1_x":225,
-                        "piece_1_y":50
+                        "piece_1_y":50,
+
+                        "piece_2_x":275,
+                        "piece_2_y":50,
+
+                        "piece_3_x":325,
+                        "piece_3_y":50,
                 },
                 // Tile 13
                 {
                         "piece_1_x":375,
-                        "piece_1_y":50
+                        "piece_1_y":50,
+
+                        "piece_2_x":425,
+                        "piece_2_y":50,
+
+                        "piece_3_x":480,
+                        "piece_3_y":50,
                 },
                 // Tile 14
                 {
                         "piece_1_x":525,
-                        "piece_1_y":50
+                        "piece_1_y":50,
+
+                        "piece_2_x":575,
+                        "piece_2_y":50,
+
+                        "piece_3_x":620,
+                        "piece_3_y":50,
                 },
                 // Tile 15
                 {
                         "piece_1_x":675,
-                        "piece_1_y":50
+                        "piece_1_y":50,
+
+                        "piece_2_x":725,
+                        "piece_2_y":50,
+
+                        "piece_3_x":770,
+                        "piece_3_y":50,
                 },
                 // Tile 16
                 {
                         "piece_1_x":850,
-                        "piece_1_y":50
+                        "piece_1_y":50,
+
+                        "piece_2_x":900,
+                        "piece_2_y":50,
+
+                        "piece_3_x":930,
+                        "piece_3_y":50,
                 },
         // Second Column
                 // Tile 17
                 {
                         "piece_1_x":835,
-                        "piece_1_y":200
+                        "piece_1_y":200,
+
+                        "piece_2_x":835,
+                        "piece_2_y":250,
+
+                        "piece_3_x":835,
+                        "piece_3_y":300,
                 },
                 // Tile 18
                 {
                         "piece_1_x":835,
-                        "piece_1_y":360
+                        "piece_1_y":360,
+
+                        "piece_2_x":835,
+                        "piece_2_y":410,
+
+                        "piece_3_x":835,
+                        "piece_3_y":525,
                 },
                 // Tile 19
                 {
                         "piece_1_x":835,
-                        "piece_1_y":520
+                        "piece_1_y":520,
+
+                        "piece_2_x":835,
+                        "piece_2_y":570,
+
+                        "piece_3_x":835,
+                        "piece_3_y":630,
+
                 },
                 // Tile 20
                 {
                         "piece_1_x":835,
-                        "piece_1_y":680
+                        "piece_1_y":680,
+
+                        "piece_2_x":835,
+                        "piece_2_y":730,
+
+                        "piece_3_x":835,
+                        "piece_3_y":770,
+
                 },
         ]
 
--- src/init.js
+++ src/init.js
@@ -3,12 +3,16 @@ window.onload = function() {
         var game_info            = new Game_info();
         var piece_controlers     = [];
         var pieces               = [];
+        var game                 = new Phaser.Game(1000, 1000, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+        var current_turn         = 0;
+        var current_piece_x;
+        var current_piece_y;
+
 
         for(var i = 0; i < number_pieces; i++){
                 piece_controlers.push(new Piece_controler());
         }
-
-        var game                 = new Phaser.Game(1000, 1000, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
         function preload () {
                 game.load.image('board_image',  game_info.board_image);
@@ -33,22 +37,30 @@ window.onload = function() {
 
                 board.anchor.setTo(0.5, 0.5);
                 leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+
+                // We need to reset these varibles so that they are correctly initalized for update
+                current_piece_x = 'piece_'+ 1 + '_x';
+                current_piece_y = 'piece_'+ 1 + '_y';
         }
 
+
+        
         function update () {
-                var current_turn    = 0;
-                var current_piece_x = 'piece_'+ 1 + '_x';
-                var current_piece_y = 'piece_'+ 1 + '_y';
-
-
                 if(leftKey.isDown){
                         piece_controlers[current_turn].current_tile++;
                         
                         if(piece_controlers[current_turn].current_tile == 20){
+                                console.log("inside the if");
                                 current_turn++;
                                 current_piece_x = 'piece_'+ (current_turn+1) + '_x';
                                 current_piece_y = 'piece_'+ (current_turn+1) + '_y';
+                                console.log(current_piece_x);
+                                console.log(current_piece_y);
                         }
+                        console.log(current_piece_x);
+                        console.log(current_piece_y);
+
+
 
                         pieces[current_turn].x = game_info.board_tiles[piece_controlers[current_turn].current_tile][current_piece_x];
                         pieces[current_turn].y = game_info.board_tiles[piece_controlers[current_turn].current_tile][current_piece_y];
