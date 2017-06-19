e4fda0cecd07fa2dcf97f24820cefbb0245d8cdf
JohnyGall <john.gallagher@tufts.edu>
2017-06-18 22:48:43 -0400

    Presentation prompts now tick down - also added next round button

--- src/presentation.js
+++ src/presentation.js
@@ -1,3 +1,14 @@
+function update_prompt () {
+        console.log(current_prompt);
+        console.log(game_info.presentation_prompts.length);
+        if(current_prompt+1 < game_info.presentation_prompts.length){
+                console.log(game_info)
+                console.log(current_prompt);
+                current_prompt++;
+                prompt_text.setText(game_info.presentation_prompts[current_prompt]);
+        }
+}
+
 var presentation_state = {
 	create: function () {
 		$("#game_canvas").show();
@@ -6,25 +17,26 @@ var presentation_state = {
 		game.stage.backgroundColor = "#FFFFFF";
 
 
-		logo                      = game.add.sprite(275, 60,'logo_image');
+		board                     = game.add.sprite(game.world.centerX, game.world.centerY, 'board_image');
+                logo                      = game.add.sprite(275, 35,'logo_image');
                 popup_background          = game.add.sprite(220, 220,'popup_background_image');
                 prompt_text               = game.add.text(350,  500, "");
+
+                
+
                 logo.scale.setTo(0.3,0.3);
-                duration_of_each_prompt   = presentation_time/5
+                board.anchor.setTo(0.5, 0.5);
+
+                duration_of_each_prompt   = (presentation_time*60000)/5
                 current_prompt = 0;
+
+                timer       = game.time.create(false);
+                timer.loop(duration_of_each_prompt, update_prompt, this);
+                timer.start();
+
 	},
 
 	update: function () {
-		timer       = game.time.create();
-        	timer_event = timer.add(Phaser.Timer.MINUTE * duration_of_each_prompt, end_of_turn, game);
-        	prompt_text.setText(game_info.presentation_prompts[current_prompt]);
-
-        	if(timer_event.delay - timer.ms == 0){
-                        current_prompt++;
-                        timer.destroy();
-                        timer = game.time.create();
-                        timer_event = timer.add(Phaser.Timer.MINUTE * duration_of_each_prompt, end_of_turn, game);   			
-                }
-		
 	}
+
 }
